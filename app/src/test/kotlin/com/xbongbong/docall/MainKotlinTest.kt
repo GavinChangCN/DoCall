6224fb6866c68190add2e7dcd597d88fcf81aad1
章华隽 <nefeed@163.com>
2017-10-11 00:30:13 +0800

    添加必要的第三方依赖包

--- app/src/test/kotlin/com/xbongbong/docall/MainKotlinTest.kt
+++ app/src/test/kotlin/com/xbongbong/docall/MainKotlinTest.kt
@@ -1,5 +1,9 @@
 package com.xbongbong.docall
 
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
 @RunWith(AndroidJUnit4::class)
 class MainKotlinTest {
     @Test
